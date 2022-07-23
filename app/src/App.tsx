e9af0e6c1773d018f3ff36ea216cfebb4c0f489f
Brandon Liu <bdon@bdon.org>
2022-07-24 02:12:49 +0800

    implement URL state for preview app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import L from "leaflet";
 import { leafletLayer } from "protomaps";
 import "leaflet/dist/leaflet.css";
 import "leaflet.sync/L.Map.Sync.js";
+import "leaflet-hash/leaflet-hash.js";
 
 const GIT_SHA = (import.meta.env.VITE_GIT_SHA || "").substr(0, 8);
 
@@ -87,21 +88,28 @@ let setLayer = (map: L.Map, optionIdx: number) => {
   }
 };
 
-function Compare(props: { closeCompare: () => void; leftMap: L.Map | null }) {
+function Compare(props: {
+  closeCompare: () => void;
+  leftMap: L.Map | null;
+  compareSelection: number;
+  setCompareSelection: (n: number) => void;
+}) {
   var map = useRef<L.Map | null>(null);
   let mapRef = useRef<HTMLDivElement | null>(null);
 
-  let [compare, setCompare] = useState<boolean>(false);
-  let [selection, setSelection] = useState<number>(0);
-
   let onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelection(+e.target.value);
+    props.setCompareSelection(+e.target.value);
   };
 
   useEffect(() => {
-    map.current = L.map(mapRef.current!).setView([0, 0], 0);
+    map.current = L.map(mapRef.current!, {
+      doubleClickZoom: false,
+      touchZoom: false,
+      scrollWheelZoom: false,
+      zoomControl: false,
+    }).setView([0, 0], 0);
     map.current.dragging.disable();
-    (props.leftMap as any).sync(map.current);
+    // (props.leftMap as any).sync(map.current);
 
     return () => {
       if (map.current) {
@@ -112,14 +120,14 @@ function Compare(props: { closeCompare: () => void; leftMap: L.Map | null }) {
   }, []);
 
   useEffect(() => {
-    if (map.current) setLayer(map.current, selection);
-  }, [selection]);
+    if (map.current) setLayer(map.current, props.compareSelection);
+  }, [props.compareSelection]);
 
   return (
     <div className="flex flex-column vh-100 w-50 bg-moon-gray">
       <div className="flex flex-grow-0 justify-between h3 items-center ph3">
         <span>
-          <select onChange={onChange}>
+          <select onChange={onChange} value={props.compareSelection}>
             {OPTIONS.map((option, i) => (
               <option value={i} key={i}>
                 {option.name}
@@ -136,12 +144,34 @@ function Compare(props: { closeCompare: () => void; leftMap: L.Map | null }) {
   );
 }
 
+let initialSelection = 0;
+let initialCompare = false;
+let initialCompareSelection = 0;
+const paramMap = new URLSearchParams(location.search).get("map");
+const paramCompare = new URLSearchParams(location.search).get("compare");
+
+if (paramMap) {
+  let idx = OPTIONS.findIndex((o) => o.name === paramMap);
+  if (idx >= 0) initialSelection = idx;
+}
+
+if (paramCompare) {
+  let idx = OPTIONS.findIndex((o) => o.name === paramCompare);
+  if (idx >= 0) {
+    initialCompare = true;
+    initialCompareSelection = idx;
+  }
+}
+
 function App() {
   var map = useRef<L.Map | null>(null);
   let mapRef = useRef<HTMLDivElement | null>(null);
 
   let [compare, setCompare] = useState<boolean>(false);
-  let [selection, setSelection] = useState<number>(0);
+  let [selection, setSelection] = useState<number>(initialSelection);
+  let [compareSelection, setCompareSelection] = useState<number>(
+    initialCompareSelection
+  );
 
   let onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelection(+e.target.value);
@@ -149,15 +179,27 @@ function App() {
 
   useEffect(() => {
     map.current = L.map(mapRef.current!).setView([0, 0], 0);
+    (map.current as any).addHash();
 
     return () => {
-      if (map.current) map.current.remove();
+      if (map.current) {
+        map.current.remove();
+        (map.current as any).removeHash();
+      }
     };
   }, []);
 
   useEffect(() => {
     if (map.current) setLayer(map.current, selection);
   }, [selection]);
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("map", OPTIONS[selection].name);
+    if (compare)
+      url.searchParams.set("compare", OPTIONS[compareSelection].name);
+    history.pushState(null, "", url.toString());
+  }, [compare, selection, compareSelection]);
 
   let leftClass = "flex flex-column vh-100 " + (compare ? "w-50" : "w-100");
 
@@ -167,7 +209,7 @@ function App() {
         <div className="flex flex-grow-0 fw6 justify-between h3 items-center ph3">
           <span>protomaps-themes {GIT_SHA}</span>
           <span>
-            <select onChange={onChange}>
+            <select onChange={onChange} value={selection}>
               {OPTIONS.map((option, i) => (
                 <option value={i} key={i}>
                   {option.name}
@@ -195,6 +237,8 @@ function App() {
           closeCompare={() => {
             setCompare(false);
           }}
+          compareSelection={compareSelection}
+          setCompareSelection={setCompareSelection}
           leftMap={map.current}
         />
       ) : null}
