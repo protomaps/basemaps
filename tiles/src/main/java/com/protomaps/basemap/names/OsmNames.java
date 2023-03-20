68d0a4944d54c77a8fbb9943c2260608a4a27fab
Brandon Liu <bdon@bdon.org>
2023-03-20 17:28:35 +0800

    add multilingual name tags from OSM

--- tiles/src/main/java/com/protomaps/basemap/names/OsmNames.java
+++ tiles/src/main/java/com/protomaps/basemap/names/OsmNames.java
@@ -2,11 +2,18 @@ package com.protomaps.basemap.names;
 
 import com.onthegomap.planetiler.FeatureCollector;
 import com.onthegomap.planetiler.reader.SourceFeature;
+import java.util.Map;
 
 public class OsmNames {
   public static FeatureCollector.Feature setOsmNames(FeatureCollector.Feature feature, SourceFeature source,
     int minzoom) {
-    feature.setAttrWithMinzoom("name", source.getTag("name"), minzoom);
+    for (Map.Entry<String, Object> tag : source.tags().entrySet()) {
+      var key = tag.getKey();
+      if (key.equals("name") || key.startsWith("name:")) {
+        feature.setAttrWithMinzoom(key, source.getTag(key), minzoom);
+      }
+    }
+
     return feature;
   }
 }
