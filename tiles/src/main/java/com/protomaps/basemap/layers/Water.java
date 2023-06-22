1dce93b0d56ca1b95d8d34851e8c1571c5f6f4f9
Nathaniel Kelso <nvkelso@gmail.com>
2023-06-22 00:21:06 -0700

    fix NE layer parsing; do NOT export names (see physical points layer instead)

--- tiles/src/main/java/com/protomaps/basemap/layers/Water.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Water.java
@@ -30,54 +30,63 @@ public class Water implements ForwardingProfile.FeatureProcessor, ForwardingProf
     var theme_min_zoom = 0;
     var theme_max_zoom = 0;
 
-    if (sourceLayer.equals("ne_110m_ocean") ) {
-      theme_min_zoom = 0;
-      theme_max_zoom = 1;
-    } else if (sourceLayer.equals("ne_110m_lakes") ) {
-      theme_min_zoom = 0;
-      theme_max_zoom = 1;
-    } else if (sourceLayer.equals("ne_50m_ocean")) {
-      theme_min_zoom = 2;
-      theme_max_zoom = 4;
-    } else if (sourceLayer.equals("ne_50m_lakes")) {
-      theme_min_zoom = 2;
-      theme_max_zoom = 4;
-    } else if (sourceLayer.equals("ne_10m_ocean")) {
-      theme_min_zoom = 5;
-      theme_max_zoom = 5;
-    } else if( sourceLayer.equals("ne_10m_lakes")) {
-      theme_min_zoom = 5;
-      theme_max_zoom = 5;
-    }
-
-    switch (sf.getString("featurecla")) {
-      case "Alkaline Lake" -> {
-        kind = "lake";
-        alkaline = 1;
+    // Only process certain Natural Earth layers
+    // Notably the landscan derived urban areas theme causes problems otherwise
+    if (sourceLayer.equals("ne_110m_ocean") || sourceLayer.equals("ne_110m_lakes") || sourceLayer.equals("ne_50m_ocean") || sourceLayer.equals("ne_50m_lakes") || sourceLayer.equals("ne_10m_ocean") || sourceLayer.equals("ne_10m_lakes") ) {
+      if (sourceLayer.equals("ne_110m_ocean")) {
+        theme_min_zoom = 0;
+        theme_max_zoom = 1;
+      } else if (sourceLayer.equals("ne_110m_lakes")) {
+        theme_min_zoom = 0;
+        theme_max_zoom = 1;
+      } else if (sourceLayer.equals("ne_50m_ocean")) {
+        theme_min_zoom = 2;
+        theme_max_zoom = 4;
+      } else if (sourceLayer.equals("ne_50m_lakes")) {
+        theme_min_zoom = 2;
+        theme_max_zoom = 4;
+      } else if (sourceLayer.equals("ne_10m_ocean")) {
+        theme_min_zoom = 5;
+        theme_max_zoom = 5;
+      } else if (sourceLayer.equals("ne_10m_lakes")) {
+        theme_min_zoom = 5;
+        theme_max_zoom = 5;
       }
-      case "Lake" -> kind = "lake";
-      case "Reservoir" -> {
-        kind = "lake";
-        reservoir = 1;
-      }
-      case "Playa" -> kind = "playa";
-      case "Ocean" -> kind = "ocean";
-    }
 
-    if (kind != "" && sf.hasTag("min_zoom")) {
-      var feature = features.polygon(this.name())
-              .setAttr("pmap:kind", kind)
-              .setAttr("pmap:min_zoom", sf.getLong("min_zoom"))
-              .setZoomRange(sf.getString("min_zoom") == null ? theme_min_zoom : (int) Double.parseDouble(sf.getString("min_zoom")), theme_max_zoom)
-              .setMinPixelSize(3.0)
-              .setBufferPixels(8);
+      switch (sf.getString("featurecla")) {
+        case "Alkaline Lake" -> {
+          kind = "lake";
+          alkaline = 1;
+        }
+        case "Lake" -> kind = "lake";
+        case "Reservoir" -> {
+          kind = "lake";
+          reservoir = 1;
+        }
+        case "Playa" -> kind = "playa";
+        case "Ocean" -> kind = "ocean";
+      }
+
+      if (kind != "" && sf.hasTag("min_zoom")) {
+        var feature = features.polygon(this.name())
+                .setAttr("pmap:kind", kind)
+                .setAttr("pmap:min_zoom", sf.getLong("min_zoom"))
+                .setZoomRange(sf.getString("min_zoom") == null ? theme_min_zoom : (int) Double.parseDouble(sf.getString("min_zoom")), theme_max_zoom)
+                .setMinPixelSize(3.0)
+                .setBufferPixels(8);
+      }
     }
   }
 
   @Override
   public void processFeature(SourceFeature sf, FeatureCollector features) {
-    if (sf.canBePolygon() && (sf.hasTag("water") || sf.hasTag("waterway") || sf.hasTag("natural", "water") ||
-      sf.hasTag("landuse", "reservoir") || sf.hasTag("leisure", "swimming_pool"))) {
+    if (sf.canBePolygon() && (
+              sf.hasTag("water") ||
+              sf.hasTag("waterway") ||
+              sf.hasTag("natural", "water") ||
+              sf.hasTag("landuse", "reservoir") ||
+              sf.hasTag("leisure", "swimming_pool")))
+    {
       var feature = features.polygon(this.name())
         .setAttr("natural", sf.getString("natural"))
         .setAttr("landuse", sf.getString("landuse"))
@@ -111,7 +120,8 @@ public class Water implements ForwardingProfile.FeatureProcessor, ForwardingProf
       }
       feature.setAttr("pmap:kind", kind);
 
-      OsmNames.setOsmNames(feature, sf, 0);
+      // NOTE: water labels for polygons are found in the physical_point layer (see also physical_line layer)
+      //OsmNames.setOsmNames(feature, sf, 0);
     }
   }
 
