89cc47cce40ce212260af5eeaa46f79d58a24dd3
Nathaniel Kelso <nvkelso@gmail.com>
2023-06-22 00:12:18 -0700

    block {building|building:part}=no; push building:part to later zooms; merge buildings at z14; note future height quantization

--- tiles/src/main/java/com/protomaps/basemap/layers/Buildings.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Buildings.java
@@ -22,8 +22,12 @@ public class Buildings implements ForwardingProfile.FeatureProcessor, Forwarding
 
   @Override
   public void processFeature(SourceFeature sf, FeatureCollector features) {
-    if (sf.canBePolygon() && (sf.hasTag("building") || sf.hasTag("building:part"))) {
+    if (sf.canBePolygon() && (
+            ( sf.hasTag("building") && !sf.hasTag("building", "no")) ||
+            ( sf.hasTag("building:part") && !sf.hasTag("building:part", "no"))))
+    {
       Double height = parseDoubleOrNull(sf.getString("height"));
+      Integer min_zoom = 10;
 
       if (height == null) {
         Double levels = parseDoubleOrNull(sf.getString("building:levels"));
@@ -32,14 +36,22 @@ public class Buildings implements ForwardingProfile.FeatureProcessor, Forwarding
         }
       }
 
+      // Limit building:part features to later zooms
+      // TODO: (nvkelso 20230621) this should be based on area and volume, too
+      if( sf.hasTag("building:part") ) {
+        min_zoom = 14;
+      }
+
       var feature = features.polygon(this.name())
         .setId(FeatureId.create(sf))
         .setAttrWithMinzoom("building:part", sf.getString("building:part"), 13)
         .setAttrWithMinzoom("layer", sf.getString("layer"), 13)
         .setAttrWithMinzoom("height", height, 13)
-        .setZoomRange(10, 15);
+        .setZoomRange(min_zoom, 15);
 
-      OsmNames.setOsmNames(feature, sf, 13);
+      // Names should mostly just be for POIs
+      // Sometimes building name and address are useful items, but only at zoom 17+
+      //OsmNames.setOsmNames(feature, sf, 13);
     }
   }
 
@@ -50,8 +62,17 @@ public class Buildings implements ForwardingProfile.FeatureProcessor, Forwarding
     }
     items = Area.filterArea(items, 0);
 
-    if (zoom >= 14)
+    // DEBUG
+    //items = Area.addAreaTag(items);
+
+    if (zoom >= 15)
       return items;
+
+    // TODO: (nvkelso 20230621) quantize height by zoom to facilitate better feature merging
+    // at zoom 12 round height to nearest 20 meters
+    // at zoom 13 round height to nearest 10 meters
+    // at zoom 14 round height to nearest 5 meters
+
     return FeatureMerge.mergeNearbyPolygons(items, 3.125, 3.125, 0.5, 0.5);
   }
 }
