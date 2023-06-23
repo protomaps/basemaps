153a6bdbb8e56962bf47c7e5b4afafe44009bf8e
Nathaniel Kelso <nvkelso@gmail.com>
2023-06-23 01:03:56 -0700

    show rivers earlier; always set kind value; intermittent to boolean

--- tiles/src/main/java/com/protomaps/basemap/layers/PhysicalLine.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/PhysicalLine.java
@@ -18,29 +18,35 @@ public class PhysicalLine implements ForwardingProfile.FeatureProcessor, Forward
   @Override
   public void processFeature(SourceFeature sf, FeatureCollector features) {
     if (sf.canBeLine() && (sf.hasTag("waterway") ||
-      sf.hasTag("natural", "strait", "cliff")) && (!sf.hasTag("waterway", "riverbank", "reservoir"))) {
+      sf.hasTag("natural", "strait", "cliff")) && (!sf.hasTag("waterway", "riverbank", "reservoir")))
+    {
+      int min_zoom = 12;
+      String kind = "other";
+      if (sf.hasTag("waterway")) {
+        kind = sf.getString("waterway");
+        if (sf.hasTag("waterway","river")) {
+          min_zoom = 9;
+        }
+      } else if (sf.hasTag("natural")) {
+        kind = sf.getString("natural");
+      }
+
       var feat = features.line(this.name())
         .setId(FeatureId.create(sf))
+        // Core Tilezen schema properties
+        .setAttr("pmap:kind", kind)
+        // Core OSM tags for different kinds of places
         .setAttr("waterway", sf.getString("waterway"))
         .setAttr("natural", sf.getString("natural"))
         // Add less common attributes only at higher zooms
         .setAttrWithMinzoom("bridge", sf.getString("bridge"), 12)
         .setAttrWithMinzoom("tunnel", sf.getString("tunnel"), 12)
         .setAttrWithMinzoom("layer", sf.getString("layer"), 12)
-        .setZoomRange(12, 15);
+        .setZoomRange(min_zoom, 15);
 
       if (sf.hasTag("intermittent", "yes")) {
-        feat.setAttr("intermittent", 1);
+        feat.setAttr("intermittent", true);
       }
-
-      String kind = "other";
-      if (sf.hasTag("waterway")) {
-        kind = "waterway";
-      } else if (sf.hasTag("natural")) {
-        kind = "natural";
-      }
-
-      feat.setAttr("pmap:kind", kind);
 
       OsmNames.setOsmNames(feat, sf, 0);
     }
