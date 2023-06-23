55c1dbffa5a71d6c5fc2a91759aa6615a753b311
Nathaniel Kelso <nvkelso@gmail.com>
2023-06-23 01:13:50 -0700

    show airport runway, taxiway earlier; show some types of rail later; smarter kind calculation

--- tiles/src/main/java/com/protomaps/basemap/layers/Transit.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Transit.java
@@ -27,6 +27,12 @@ public class Transit implements ForwardingProfile.FeatureProcessor, ForwardingPr
 
       int minzoom = 11;
 
+      if (sf.hasTag("aeroway", "runway")) {
+        minzoom = 9;
+      } else
+      if (sf.hasTag("aeroway", "taxiway")) {
+        minzoom = 10;
+      } else
       if (sf.hasTag("service", "yard", "siding", "crossover")) {
         minzoom = 13;
       } else
@@ -34,35 +40,61 @@ public class Transit implements ForwardingProfile.FeatureProcessor, ForwardingPr
         minzoom = 13;
       }
 
-      var feature = features.line(this.name())
-        .setId(FeatureId.create(sf))
-        .setAttr("railway", sf.getString("railway"))
-        .setAttr("route", sf.getString("route"))
-        .setAttr("aeroway", sf.getString("aeroway"))
-        .setAttr("man_made", sf.getString("pier"))
-        .setAttr("service", sf.getString("service"))
-        .setAttr("aerialway", sf.getString("aerialway"))
-        .setAttr("network", sf.getString("network"))
-        .setAttr("ref", sf.getString("ref"))
-        .setAttr("highspeed", sf.getString("highspeed"))
-        .setAttr("layer", sf.getString("layer"))
-        .setZoomRange(minzoom, 15);
-
       String kind = "other";
+      String kind_detail = "other";
       if (sf.hasTag("aeroway")) {
         kind = "aeroway";
+        kind_detail = sf.getString("aeroway");
+      } else if (sf.hasTag("railway", "disused", "funicular", "light_rail", "miniature", "monorail", "narrow_gauge", "preserved", "subway", "tram")) {
+        kind = "rail";
+        kind_detail = sf.getString("railway");
+        minzoom = 14;
       } else if (sf.hasTag("railway")) {
-        kind = "railway";
+        kind = "rail";
+        kind_detail = sf.getString("railway");
+
+        if( kind_detail.equals("service") ) {
+          minzoom = 13;
+
+          // eg a rail yard
+          if( sf.hasTag("service") ) {
+            minzoom = 14;
+          }
+        }
       } else if (sf.hasTag("ferry")) {
         kind = "ferry";
+        kind_detail = sf.getString("ferry");
       } else if (sf.hasTag("man_made", "pier")) {
         kind = "pier";
-        feature.setMinPixelSize(2);
       } else if (sf.hasTag("aerialway")) {
         kind = "aerialway";
+        kind_detail = sf.getString("aerialway");
       }
 
-      feature.setAttr("pmap:kind", kind);
+      var feature = features.line(this.name())
+        .setId(FeatureId.create(sf))
+        // Core Tilezen schema properties
+        .setAttr("pmap:kind", kind)
+        // Core OSM tags for different kinds of places
+        .setAttr("aerialway", sf.getString("aerialway"))
+        .setAttr("aeroway", sf.getString("aeroway"))
+        .setAttr("highspeed", sf.getString("highspeed"))
+        .setAttr("layer", sf.getString("layer"))
+        .setAttr("man_made", sf.getString("pier"))
+        .setAttr("network", sf.getString("network"))
+        .setAttr("railway", sf.getString("railway"))
+        .setAttr("ref", sf.getString("ref"))
+        .setAttr("route", sf.getString("route"))
+        .setAttr("service", sf.getString("service"))
+        .setZoomRange(minzoom, 15);
+
+      if( kind_detail != "other" ) {
+        feature.setAttr("pmap:kind_detail", kind_detail);
+      }
+
+      if( kind == "pier" ) {
+        feature.setMinPixelSize(2);
+      }
 
       OsmNames.setOsmNames(feature, sf, 0);
     }
