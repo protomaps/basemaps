6e575e85e2695746b414cac6a92c5a07197ba6a0
Brandon Liu <bdon@bdon.org>
2023-07-13 22:14:31 +0800

    Improve LinkSimplify to work less aggressively and handle edge cases [#53] * allow multiple ways in connectors. * allow connectors to touch a highway at any point, not just endpoints.

--- tiles/src/main/java/com/protomaps/basemap/postprocess/LinkSimplify.java
+++ tiles/src/main/java/com/protomaps/basemap/postprocess/LinkSimplify.java
@@ -1,73 +1,90 @@
 package com.protomaps.basemap.postprocess;
 
+import com.carrotsearch.hppc.IntArrayList;
+import com.carrotsearch.hppc.IntObjectMap;
+import com.carrotsearch.hppc.IntStack;
+import com.carrotsearch.hppc.cursors.IntCursor;
 import com.onthegomap.planetiler.VectorTile;
+import com.onthegomap.planetiler.archive.WriteableTileArchive;
+import com.onthegomap.planetiler.collection.Hppc;
 import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.geo.GeometryType;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
+
+import com.onthegomap.planetiler.mbtiles.Mbtiles;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.operation.linemerge.LineMerger;
+
 
 public class LinkSimplify {
-  public static List<VectorTile.Feature> linkSimplify(List<VectorTile.Feature> items, String key, String mainval,
-    String linkval) {
+
+  /**
+   * Post-processing to remove "hairballs" from road networks.
+   * <p>
+   * OpenStreetMap uses the highway=motorway_link tag to connect motorways to motorways, as well
+   * as connect motorways to lower-class highway features. If we include all ways at all zooms, motorways
+   * will grow "hairballs" once the lower-class features are generalized away.
+   * <p>
+   * LinkSimplify uses some very basic heuristics to distinguish "connectors" and "offramps":
+   * If a link way's 2 endpoints both touch (a motorway at any point | another link at an endpoint), keep it.
+   * (we're not guaranteed connectors are a single way, it can be multiple)
+   * Otherwise throw it away (an offramp)
+   * <p>
+   * The above logic generalizes to any passed tag, not just highway=motorway | motorway_link.
+   * </p>
+   */
+  public static List<VectorTile.Feature> linkSimplify(List<VectorTile.Feature> items, String key, String mainval, String linkval) throws GeometryException {
+
     Map<Coordinate, Integer> degrees = new HashMap<>();
 
     for (VectorTile.Feature item : items) {
-      if (item.geometry().geomType() == GeometryType.LINE && item.attrs().get(key).equals(linkval)) {
-        try {
+      if (item.geometry().geomType() == GeometryType.LINE) {
+        if (item.attrs().get(key).equals(linkval)) {
           Coordinate[] coordinates = item.geometry().decode().getCoordinates();
-          if (coordinates.length == 0)
-            continue;
-          Coordinate start = coordinates[0];
-          Coordinate end = coordinates[coordinates.length - 1];
-          degrees.put(start, 0);
-          degrees.put(end, 0);
-        } catch (GeometryException e) {
-
-        }
-      }
-    }
-
-    for (VectorTile.Feature item : items) {
-      if (item.geometry().geomType() == GeometryType.LINE && item.attrs().get(key).equals(mainval)) {
-        try {
-          Coordinate[] coordinates = item.geometry().decode().getCoordinates();
-          if (coordinates.length == 0)
-            continue;
+          if (coordinates.length == 0) continue;
           Coordinate start = coordinates[0];
           Coordinate end = coordinates[coordinates.length - 1];
           if (degrees.containsKey(start)) {
             degrees.put(start, degrees.get(start) + 1);
+          } else {
+            degrees.put(start, 1);
           }
           if (degrees.containsKey(end)) {
             degrees.put(end, degrees.get(end) + 1);
+          } else {
+            degrees.put(end, 1);
           }
-        } catch (GeometryException e) {
+
+        } else if (item.attrs().get(key).equals(mainval)) {
+          Coordinate[] coordinates = item.geometry().decode().getCoordinates();
+          for (Coordinate c : coordinates) {
+            if (degrees.containsKey(c)) {
+              degrees.put(c, degrees.get(c) + 1);
+            } else {
+              degrees.put(c, 1);
+            }
+          }
         }
       }
     }
 
-    List<VectorTile.Feature> retval = new ArrayList<>();
+    List<VectorTile.Feature> output = new ArrayList<>();
 
     for (VectorTile.Feature item : items) {
       if (item.geometry().geomType() == GeometryType.LINE && item.attrs().get(key).equals(linkval)) {
-        try {
-          Coordinate[] coordinates = item.geometry().decode().getCoordinates();
-          if (coordinates.length == 0)
-            continue;
-          Coordinate start = coordinates[0];
-          Coordinate end = coordinates[coordinates.length - 1];
-          if (degrees.get(start) == 1 && degrees.get(end) == 1) {
-            retval.add(item);
-          }
-        } catch (GeometryException e) {
+        Coordinate[] coordinates = item.geometry().decode().getCoordinates();
+        if (coordinates.length == 0) continue;
+        Coordinate start = coordinates[0];
+        Coordinate end = coordinates[coordinates.length - 1];
+        if (degrees.get(start) >= 2 && degrees.get(end) >= 2) {
+          output.add(item);
         }
       } else {
-        retval.add(item);
+        output.add(item);
       }
     }
-    return retval;
+    return output;
   }
 }
