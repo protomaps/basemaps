cd0a08b9c37f952eddcca4c41c5021ac2931558b
Brandon Liu <bdon@users.noreply.github.com>
2023-07-21 21:04:59 +0800

    add osmEarthWaterBounds argument (#73)
    * add osmEarthWaterBounds argument to limit prepared OSM features to a bounding box.
    
    This enables efficient generation of planet tiles at zooms based on NE, and prepared+raw OSM from small areas at high zooms.

--- tiles/src/test/java/com/protomaps/basemap/postprocess/LinkSimplifyTest.java
+++ tiles/src/test/java/com/protomaps/basemap/postprocess/LinkSimplifyTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-public class LinkSimplifyTest {
+class LinkSimplifyTest {
 
   @Test
   void testNoop() throws GeometryException {
