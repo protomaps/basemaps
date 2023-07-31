f3108991d75d15215f041b1f059ba8993ce464b2
Nathaniel Kelso <nvkelso@gmail.com>
2023-07-31 15:07:25 -0700

    also test new wikidata path

--- tiles/src/test/java/com/protomaps/basemap/feature/CountryInfosTest.java
+++ tiles/src/test/java/com/protomaps/basemap/feature/CountryInfosTest.java
@@ -10,35 +10,51 @@ import org.junit.jupiter.api.Test;
 class CountryInfosTest {
 
   @Test
-  void testLookupUS() {
-    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "US", "ISO3166-1:alpha2", "US", "name", "United States"), "testsource", null, 0);
+  void testLookupUSbyISO() {
+    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "US", "ISO3166-1:alpha2", "US", "name", "United States", "wikidata", "Q30"), "testsource", null, 0);
     var info = CountryInfos.getByISO(sf);
-    assertEquals(2.5, info.minZoom());
-    assertEquals(6.5, info.maxZoom());
+    assertEquals(0.7, info.minZoom());
+    assertEquals(4.7, info.maxZoom());
   }
 
   @Test
-  void testLookupMX() {
-    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "MX", "ISO3166-1:alpha2", "MX", "name", "México"), "testsource", null, 0);
+  void testLookupUSbyWikidata() {
+    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "US", "ISO3166-1:alpha2", "US", "name", "United States", "wikidata", "Q30"), "testsource", null, 0);
+    var info = CountryInfos.getByWikidata(sf);
+    assertEquals(0.7, info.minZoom());
+    assertEquals(4.7, info.maxZoom());
+  }
+
+  @Test
+  void testLookupMXbyISO() {
+    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "MX", "ISO3166-1:alpha2", "MX", "name", "México", "wikidata", "Q96"), "testsource", null, 0);
     var info = CountryInfos.getByISO(sf);
-    assertEquals(5.9, info.minZoom());
-    assertEquals(10.2, info.maxZoom());
+    assertEquals(1.0, info.minZoom());
+    assertEquals(5.7, info.maxZoom());
+  }
+
+  @Test
+  void testLookupMXbyWikidata() {
+    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1_alpha_2", "MX", "ISO3166-1:alpha2", "MX", "name", "México", "wikidata", "Q96"), "testsource", null, 0);
+    var info = CountryInfos.getByWikidata(sf);
+    assertEquals(1.0, info.minZoom());
+    assertEquals(5.7, info.maxZoom());
   }
 
   @Test
   void testAlternateIsoTag() {
-    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1", "MX", "ISO3166-1", "MX", "name", "México"), "testsource",
+    var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1", "MX", "ISO3166-1", "MX", "name", "México", "wikidata", "Q96"), "testsource",
       null, 0);
     var info = CountryInfos.getByISO(sf);
-    assertEquals(5.9, info.minZoom());
-    assertEquals(10.2, info.maxZoom());
+    assertEquals(1.0, info.minZoom());
+    assertEquals(5.7, info.maxZoom());
   }
 
   @Test
   void testNotFoundCountryIso() {
     var sf = SimpleFeature.create(GeoUtils.EMPTY_POINT, Map.of("country_code_iso3166_1", "XX", "ISO3166-1:alpha2", "XX", "name", "Null Island trap street"), "testsource", null, 0);
     var info = CountryInfos.getByISO(sf);
-    assertEquals(8.0, info.minZoom());
-    assertEquals(11.0, info.maxZoom());
+    assertEquals(7.0, info.minZoom());
+    assertEquals(10.0, info.maxZoom());
   }
 }
