d1497eb5474d6179bb8905d8ee8b0dbb9fb30683
Brandon Liu <bdon@bdon.org>
2023-09-16 19:46:59 +0800

    ugh

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        mapview: resolve(__dirname, 'index.html'),
         builds: resolve(__dirname, 'builds/index.html'),
         visualtests: resolve(__dirname, 'visualtests/index.html')
       },
