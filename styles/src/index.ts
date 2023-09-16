3995649b4e85c62593afa2a3feeae53d627c8940
Brandon Liu <bdon@bdon.org>
2023-09-16 10:47:44 +0800

    move base dir to styles/ [#121]
    * remove debug style, as that is redundant with pmtiles viewer.

--- styles/src/index.ts
+++ styles/src/index.ts
@@ -1,9 +1,7 @@
-import debug_layers from "./debug_layers";
 import { nolabels_layers, labels_layers } from "./base_layers";
 import colors from "./colors";
 
 export default function (source: string, variant: string) {
-    if (variant == "debug") return debug_layers(source);
     let theme = (colors as any)[variant];
     return nolabels_layers(source, theme).concat(labels_layers(source, theme));
 }
