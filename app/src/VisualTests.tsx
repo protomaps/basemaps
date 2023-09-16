80588ae40d49c8b84580de895bba7bbe49c75f37
Brandon Liu <bdon@users.noreply.github.com>
2023-09-16 20:22:11 +0800

    New app (#123)
    * Remove Github actions visual CI in favor of daily build [#121]
    
    * new app skeleton [#122]
    
    * Flesh out map view component with themes and JSON export [#121]
    
    * openlayers in new app [#121]
    
    * port functionality from base/index.html [#121]
    
    * new GH action for app build, remove cruft [#121]

---
+++ app/src/VisualTests.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VisualTestsComponent from './VisualTestsComponent.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <VisualTestsComponent/>
+  </React.StrictMode>,
+)
