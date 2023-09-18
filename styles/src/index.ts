38a6077c03e7c576ba6c4299c4f8789406ba9842
Brandon Liu <bdon@bdon.org>
2023-09-18 17:52:45 +0800

    Remove use of any types for styles; refactor styles files

--- styles/src/index.ts
+++ styles/src/index.ts
@@ -1,17 +1,18 @@
 import { nolabels_layers, labels_layers } from "./base_layers";
-import colors from "./colors";
+import themes from "./themes";
+import { LayerSpecification } from "@maplibre/maplibre-gl-style-spec";
 
-export default function (source: string, variant: string) {
-    let theme = (colors as any)[variant];
+export default function (source: string, key: string): LayerSpecification[] {
+    let theme = themes[key];
     return nolabels_layers(source, theme).concat(labels_layers(source, theme));
 }
 
-export function noLabels(source: string, variant: string) {
-    let theme = (colors as any)[variant];
+export function noLabels(source: string, key: string): LayerSpecification[] {
+    let theme = themes[key];
     return nolabels_layers(source, theme);
 }
 
-export function labels(source: string, variant: string) {
-    let theme = (colors as any)[variant];
+export function labels(source: string, key: string): LayerSpecification[] {
+    let theme = themes[key];
     return labels_layers(source, theme);
 }
