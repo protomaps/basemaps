7b3b3a781584ea8d80106881553ce0f86bd1e6b5
Brandon Liu <bdon@bdon.org>
2023-09-21 18:12:57 +0800

    re-implement maperture comparison [#121]

--- app/src/BuildsComponent.tsx
+++ app/src/BuildsComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Dispatch, SetStateAction } from "react";
 
 interface Build {
   uploaded: string;
@@ -25,14 +25,46 @@ function formatBytes(bytes: number, decimals = 2) {
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
 
-function BuildComponent(props: { build: Build }) {
+function BuildComponent(props: {
+  build: Build;
+  idx: number;
+  cmpA: number;
+  cmpB: number;
+  setCmpA: Dispatch<SetStateAction<number>>;
+  setCmpB: Dispatch<SetStateAction<number>>;
+}) {
   const build = props.build;
   const link = `https://build.protomaps.com/${build.key}`;
+  const idx = props.idx;
+
+  const onChangeA = () => {
+    props.setCmpA(idx);
+  };
+
+  const onChangeB = () => {
+    props.setCmpB(idx);
+  };
+
   return (
     <tr>
       <td>
-        <input type="radio" />
-        <input type="radio" />
+        <span style={{display: "inline-block", width:"20px"}}>
+          { idx > props.cmpB &&
+          <input
+            type="radio"
+            onChange={onChangeA}
+            checked={idx === props.cmpA}
+          /> }
+        </span>
+        <span style={{display: "inline-block", width:"20px"}}>
+          { idx < props.cmpA &&
+          <input
+            type="radio"
+            onChange={onChangeB}
+            checked={idx === props.cmpB}
+          /> }
+        </span>
+
       </td>
       <td>{build.key}</td>
       <td>{formatBytes(build.size)}</td>
@@ -47,8 +79,56 @@ function BuildComponent(props: { build: Build }) {
   );
 }
 
+interface MaperturePayload {
+  name: string;
+  type: string;
+  renderer: string;
+  url: string;
+  index: number;
+}
+
+// [
+//   {
+//     "name":"20230101",
+//     "type":"maplibre-gl",
+//     "renderer":"maplibre-gl",
+//     "url":"https://build-metadata.protomaps.dev/style@2.0.0-alpha.0+theme@dark+tiles@20230920.json",
+//     "index":0
+//   },
+//   {"id":"outdoors-v11","name":"Mapbox Outdoors","type":"mapbox-gl","url":"mapbox://styles/mapbox/outdoors-v11","index":1,"renderer":"mapbox-gl"}
+// ]
+
 export default function BuildsComponent() {
   const [builds, setBuilds] = useState<Build[]>([]);
+
+  // the index is starting from the top (0)
+  const [cmpA, setCmpA] = useState<number>(1);
+  const [cmpB, setCmpB] = useState<number>(0);
+
+  const style = "2.0.0-alpha.0";
+  const theme = "light";
+
+
+  const openMaperture = () => {
+    const leftKey = builds[cmpA].key.replace(".pmtiles","");
+    const rightKey = builds[cmpB].key.replace(".pmtiles","");
+    const left:MaperturePayload = {
+      name: `${leftKey} ${style} ${theme}`,
+      type: 'maplibre-gl',
+      renderer: 'maplibre-gl',
+      index: 0,
+      url: `https://build-metadata.protomaps.dev/style@${style}+theme@${theme}+tiles@${leftKey}.json`
+    };
+    const right:MaperturePayload = {
+      name: `${rightKey} ${style} ${theme}`,
+      type: 'maplibre-gl',
+      renderer: 'maplibre-gl',
+      index: 0,
+      url: `https://build-metadata.protomaps.dev/style@${style}+theme@${theme}+tiles@${rightKey}.json`
+    };
+    const payload = JSON.stringify([left,right]);
+    open("https://stamen.github.io/maperture/#maps=" + encodeURIComponent(payload));
+  }
 
   useEffect(() => {
     fetch("https://build-metadata.protomaps.dev/builds.json")
@@ -56,19 +136,27 @@ export default function BuildsComponent() {
         return r.json();
       })
       .then((j) => {
-        setBuilds(j);
+        setBuilds(j.sort((a:Build,b:Build) => { return a.key < b.key }));
       });
   }, []);
 
   return (
     <div>
       <h1>Builds</h1>
-      <button>Compare Examples</button>
-      {/*<button>Compare Maperture</button>*/}
+      {/*<button>Compare Examples</button>*/}
+      <button onClick={openMaperture}>Compare in Maperture</button>
       <table>
         <tbody>
-          {builds.map((build: Build) => (
-            <BuildComponent key={build.key} build={build} />
+          {builds.map((build: Build, idx: number) => (
+            <BuildComponent
+              key={build.key}
+              build={build}
+              idx={idx}
+              cmpA={cmpA}
+              cmpB={cmpB}
+              setCmpA={setCmpA}
+              setCmpB={setCmpB}
+            />
           ))}
         </tbody>
       </table>
