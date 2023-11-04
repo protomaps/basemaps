51525aa39e0cda319f3c80d614ccd86841ff6149
Eike Send <eike.send@gmail.com>
2023-11-04 18:12:36 +0100

    Apply maven spotless again : D

--- tiles/src/main/java/com/protomaps/basemap/layers/Pois.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Pois.java
@@ -215,7 +215,7 @@ public class Pois implements ForwardingProfile.FeatureProcessor, ForwardingProfi
         kindDetail = sf.getString("station");
       }
       
-      if (sf.hasTag("railway", "station") && 
+      if (sf.hasTag("railway", "station") &&
         (!sf.hasTag("station") || sf.hasTag("station", "train", "light_rail", "subway"))) {
         minZoom = 10;
       } else if (sf.hasTag("railway", "halt", "tram_stop")) {
