397316885be7ba1777ae877a4e1079deb7b7e984
Brandon Liu <bdon@users.noreply.github.com>
2023-11-10 08:46:06 +0800

    Move tileset version into Basemap.java [#59] (#178)
    * Move tileset version into Basemap.java [#59]
    
    Also remove dead code related to old CI system for limited areas.
    
    * linting

--- tiles/src/main/java/com/protomaps/basemap/Basemap.java
+++ tiles/src/main/java/com/protomaps/basemap/Basemap.java
@@ -19,12 +19,11 @@ import com.protomaps.basemap.layers.Roads;
 import com.protomaps.basemap.layers.Transit;
 import com.protomaps.basemap.layers.Water;
 import java.nio.file.Path;
-import org.locationtech.jts.geom.Envelope;
 
 
 public class Basemap extends ForwardingProfile {
 
-  public Basemap(Envelope earthWaterBounds, NaturalEarthDb naturalEarthDb, QrankDb qrankDb) {
+  public Basemap(NaturalEarthDb naturalEarthDb, QrankDb qrankDb) {
 
     var admin = new Boundaries();
     registerHandler(admin);
@@ -69,27 +68,31 @@ public class Basemap extends ForwardingProfile {
     registerHandler(transit);
     registerSourceHandler("osm", transit);
 
-    var water = new Water(earthWaterBounds);
+    var water = new Water();
     registerHandler(water);
     registerSourceHandler("osm", water);
     registerSourceHandler("osm_water", water::processPreparedOsm);
     registerSourceHandler("ne", water::processNe);
 
-    var earth = new Earth(earthWaterBounds);
+    var earth = new Earth();
     registerHandler(earth);
     registerSourceHandler("osm_land", earth::processPreparedOsm);
     registerSourceHandler("ne", earth::processNe);
-
   }
 
   @Override
   public String name() {
-    return "Basemap";
+    return "Protomaps Basemap";
   }
 
   @Override
   public String description() {
     return "Basemap layers derived from OpenStreetMap and Natural Earth";
+  }
+
+  @Override
+  public String version() {
+    return "3.0.0-pre1";
   }
 
   @Override
@@ -114,19 +117,12 @@ public class Basemap extends ForwardingProfile {
     Path dataDir = Path.of("data");
     Path sourcesDir = dataDir.resolve("sources");
 
-    // Limit the output of prepared OSM earth/water to a bounding box.
-    // Used in combination with the `--bounds` argument to generate a complete planet based on NE at low zooms,
-    // and a complete high-zoom tileset without having to tile earth/water outside the bbox.
-    Envelope earthWaterBounds = args.bounds("osm-earth-water-bounds", "spatial bbox of osm earth+water");
-
     Path nePath = sourcesDir.resolve("natural_earth_vector.sqlite.zip");
     String neUrl = "https://naciscdn.org/naturalearth/packages/natural_earth_vector.sqlite.zip";
 
     String area = args.getString("area", "geofabrik area to download", "monaco");
 
     var planetiler = Planetiler.create(args)
-      // (nvkelso 20230817) Order of operations matters here so all NE places can be added to RTree indexes
-      //                    before OSM uses them for data joins
       .addNaturalEarthSource("ne", nePath, neUrl)
       .addOsmSource("osm", Path.of("data", "sources", area + ".osm.pbf"), "geofabrik:" + area)
       .addShapefileSource("osm_water", sourcesDir.resolve("water-polygons-split-3857.zip"),
@@ -141,7 +137,7 @@ public class Basemap extends ForwardingProfile {
     var naturalEarthDb = NaturalEarthDb.fromSqlite(nePath, tmpDir);
     var qrankDb = QrankDb.fromCsv(sourcesDir.resolve("qrank.csv.gz"));
 
-    planetiler.setProfile(new Basemap(earthWaterBounds, naturalEarthDb, qrankDb)).setOutput(Path.of(area + ".pmtiles"))
+    planetiler.setProfile(new Basemap(naturalEarthDb, qrankDb)).setOutput(Path.of(area + ".pmtiles"))
       .run();
   }
 }
--- tiles/src/main/java/com/protomaps/basemap/layers/Water.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Water.java
@@ -1,7 +1,5 @@
 package com.protomaps.basemap.layers;
 
-import static com.protomaps.basemap.feature.SpatialFilter.withinBounds;
-
 import com.onthegomap.planetiler.FeatureCollector;
 import com.onthegomap.planetiler.FeatureMerge;
 import com.onthegomap.planetiler.ForwardingProfile;
@@ -11,27 +9,18 @@ import com.onthegomap.planetiler.reader.SourceFeature;
 import com.onthegomap.planetiler.util.Parse;
 import com.protomaps.basemap.postprocess.Area;
 import java.util.List;
-import org.locationtech.jts.geom.Envelope;
 
 public class Water implements ForwardingProfile.FeatureProcessor, ForwardingProfile.FeaturePostProcessor {
-
-  private final Envelope bounds;
-
-  public Water(Envelope bounds) {
-    this.bounds = bounds;
-  }
 
   @Override
   public String name() {
     return "water";
   }
 
-  public void processPreparedOsm(SourceFeature sf, FeatureCollector features) {
-    if (withinBounds(this.bounds, sf)) {
-      features.polygon(this.name())
-        .setAttr("pmap:kind", "water")
-        .setZoomRange(6, 15).setBufferPixels(8);
-    }
+  public void processPreparedOsm(SourceFeature ignoredSf, FeatureCollector features) {
+    features.polygon(this.name())
+      .setAttr("pmap:kind", "water")
+      .setZoomRange(6, 15).setBufferPixels(8);
   }
 
   public void processNe(SourceFeature sf, FeatureCollector features) {
