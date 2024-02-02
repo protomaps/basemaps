af7c9e9a4ba1e6c9eb14b2a566e10d98cdbd0e88
Lena Brüder <oss@lena-brueder.de>
2024-02-02 17:40:58 +0100

    Closes #204 and adds POIs for landuse=village_green, laduse=allotments, leisure=playground (#212)

--- tiles/src/test/java/com/protomaps/basemap/layers/PoisTest.java
+++ tiles/src/test/java/com/protomaps/basemap/layers/PoisTest.java
@@ -34,4 +34,48 @@ class PoisTest extends LayerTest {
         0
       )));
   }
+
+  @Test
+  void allotments() {
+    // this test shows two list elements because we're running against the whole profile, which means we're getting
+    // results form multiple layer classes. This may mean the test breaks when other layer classes are changed.
+    assertFeatures(15,
+      List.of(Map.of("pmap:kind", "allotments"),
+        Map.of("pmap:kind", "allotments", "pmap:min_zoom", 16, "name", "Kleingartenverein Kartoffel")),
+      process(SimpleFeature.create(
+        newPolygon(0, 0, 0, 1, 1, 1, 1, 0, 0, 0),
+        new HashMap<>(Map.of("landuse", "allotments", "name", "Kleingartenverein Kartoffel")),
+        "osm",
+        null,
+        0
+      )));
+  }
+
+  @Test
+  void villageGreen() {
+    assertFeatures(15,
+      List.of(Map.of("pmap:kind", "village_green"),
+        Map.of("pmap:kind", "village_green", "pmap:min_zoom", 8, "name", "Stadtpark Eiche")),
+      process(SimpleFeature.create(
+        newPolygon(0, 0, 0, 1, 1, 1, 1, 0, 0, 0),
+        new HashMap<>(Map.of("landuse", "village_green", "name", "Stadtpark Eiche")),
+        "osm",
+        null,
+        0
+      )));
+  }
+
+  @Test
+  void playground() {
+    assertFeatures(15,
+      List.of(Map.of("pmap:kind", "playground"),
+        Map.of("pmap:kind", "playground", "pmap:min_zoom", 18, "name", "Spielwiese")),
+      process(SimpleFeature.create(
+        newPolygon(0, 0, 0, 1, 1, 1, 1, 0, 0, 0),
+        new HashMap<>(Map.of("leisure", "playground", "name", "Spielwiese")),
+        "osm",
+        null,
+        0
+      )));
+  }
 }
