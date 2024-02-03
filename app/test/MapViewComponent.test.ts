f1331942c7b3ad934e39cfb62965f4d81a91472e
Brandon Liu <bdon@bdon.org>
2024-02-03 14:00:59 +0800

    fix loading of TileJSON in viewer

--- app/test/MapViewComponent.test.ts
+++ app/test/MapViewComponent.test.ts
@@ -1,10 +1,10 @@
-import { isValidTiles } from "src/MapViewComponent";
+import { isValidPMTiles } from "src/MapViewComponent";
 import { expect, test } from "vitest";
 
 test("checks valid tiles value", () => {
-  expect(isValidTiles("local.pmtiles")).toBe(true);
-  expect(isValidTiles("http://example.com/remote.pmtiles")).toBe(true);
-  expect(isValidTiles("http://example.com/remote.pmtiles?abc=def")).toBe(true);
-  expect(isValidTiles("invalid")).toBe(false);
-  expect(isValidTiles("invalid.pmtiles?abc=def")).toBe(false);
+  expect(isValidPMTiles("local.pmtiles")).toBe(true);
+  expect(isValidPMTiles("http://example.com/remote.pmtiles")).toBe(true);
+  expect(isValidPMTiles("http://example.com/remote.pmtiles?abc=def")).toBe(true);
+  expect(isValidPMTiles("invalid")).toBe(false);
+  expect(isValidPMTiles("invalid.pmtiles?abc=def")).toBe(false);
 });
