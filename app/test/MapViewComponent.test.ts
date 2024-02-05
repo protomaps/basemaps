7ad630d28bf2144caf2db0a2fbc8960b760783f3
Brandon Liu <bdon@bdon.org>
2024-02-05 17:07:48 +0800

    linting

--- app/test/MapViewComponent.test.ts
+++ app/test/MapViewComponent.test.ts
@@ -4,7 +4,9 @@ import { expect, test } from "vitest";
 test("checks valid tiles value", () => {
   expect(isValidPMTiles("local.pmtiles")).toBe(true);
   expect(isValidPMTiles("http://example.com/remote.pmtiles")).toBe(true);
-  expect(isValidPMTiles("http://example.com/remote.pmtiles?abc=def")).toBe(true);
+  expect(isValidPMTiles("http://example.com/remote.pmtiles?abc=def")).toBe(
+    true,
+  );
   expect(isValidPMTiles("invalid")).toBe(false);
   expect(isValidPMTiles("invalid.pmtiles?abc=def")).toBe(false);
 });
