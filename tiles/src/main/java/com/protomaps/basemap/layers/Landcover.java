69d8c9a1433a482c31f64b1c98a457af4a1ac445
Brandon Liu <bdon@bdon.org>
2024-03-28 12:50:13 -0700

    formatting

--- tiles/src/main/java/com/protomaps/basemap/layers/Landcover.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Landcover.java
@@ -6,19 +6,24 @@ import com.onthegomap.planetiler.VectorTile;
 import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import com.protomaps.basemap.feature.FeatureId;
-
 import java.util.List;
 
 public class Landcover implements ForwardingProfile.FeaturePostProcessor {
 
   public void processLandcover(SourceFeature sf, FeatureCollector features) {
     String kind = sf.getString("class");
-    if (kind.equals("urban")) kind = "urban_area";
-    if (kind.equals("crop")) kind = "farmland";
-    if (kind.equals("grass")) kind = "grassland";
-    if (kind.equals("trees")) kind = "forest";
-    if (kind.equals("snow")) kind = "glacier";
-    if (kind.equals("shrub")) kind = "scrub";
+    if (kind.equals("urban"))
+      kind = "urban_area";
+    if (kind.equals("crop"))
+      kind = "farmland";
+    if (kind.equals("grass"))
+      kind = "grassland";
+    if (kind.equals("trees"))
+      kind = "forest";
+    if (kind.equals("snow"))
+      kind = "glacier";
+    if (kind.equals("shrub"))
+      kind = "scrub";
     // barren is passed through
 
     features.polygon(this.name())
