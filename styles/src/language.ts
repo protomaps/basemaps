fdc4f620e6c7406f319a17ba6f29ab56d99114a4
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-08-18 17:15:28 +0200

    Add comments, support Hindi

--- styles/src/language.ts
+++ styles/src/language.ts
@@ -1,3 +1,40 @@
+function get_name_block(script_segment: 'name' | 'name2' | 'name3') {
+    let script;
+
+    if (script_segment === 'name') {
+        script = 'script';
+    }
+    else if (script_segment === 'name2') {
+        script = 'script2';
+    }
+    else if (script_segment === 'name3') {
+        script = 'script3';
+    }
+
+    return [
+        ["get", `pmap:pgf:${script_segment}`],
+        {
+            "text-font": [
+                "case",
+                ["==", ["get", `pmap:${script}`], "Devanagari"],
+                ["literal", ["Noto Sans Devanagari Regular v1"]],
+                ["literal", ["Noto Sans Regular"]]
+            ]
+        }
+    ];
+}
+
+function get_font_formatting(script: string) {
+    if (script === "Devanagari") {
+        return {
+            "text-font": ["literal", ["Noto Sans Devanagari Regular v1"]]
+        };
+    }
+    else {
+        return {};
+    }
+}
+
 export function places_locality_text_field(
     lang: string,
     script: string
@@ -6,31 +43,34 @@ export function places_locality_text_field(
         "case",
         [
             "all",
-            ["has", "name"],
-            ["!", ["has", "name2"]],
-            ["!", ["has", "name3"]]
+            ["has", "pmap:pgf:name"],
+            ["!", ["has", "pmap:pgf:name2"]],
+            ["!", ["has", "pmap:pgf:name3"]]
         ],
+        // The local name has 1 script segment: `name`
         [
             "case",
             script === "Latin" ? ["has", "pmap:script"] : ["!=", ["get", "pmap:script"], script],
+            // `name` is not in the target script
             [
                 "format",
                 [
                     "coalesce",
-                    ["get", `name:${lang}`],
-                    ["get", "name:en"]
+                    ["get", `pmap:pgf:name:${lang}`],
+                    ["get", "pmap:pgf:name:en"] // Always fallback to English 
                 ],
-                {},
+                get_font_formatting(script),
                 "\n",
                 {},
                 [
                     "case",
                     [
                         "all",
-                        ["!", ["has", `name:${lang}`]],
-                        ["has", "name:en"],
+                        ["!", ["has", `pmap:pgf:name:${lang}`]],
+                        ["has", "pmap:pgf:name:en"],
                         ["!", ["has", "pmap:script"]]
                     ],
+                    // We did fallback to English in the first line and `name` is Latin
                     "",
                     ["get", "pmap:pgf:name"]
                 ],
@@ -43,18 +83,24 @@ export function places_locality_text_field(
                     ]
                 }
             ],
+            // `name` is in the target script
             [
-                "coalesce",
-                ["get", `name:${lang}`],
-                ["get", "name"]
+                "format",
+                [
+                    "coalesce",
+                    ["get", `pmap:pgf:name:${lang}`],
+                    ["get", "pmap:pgf:name"]
+                ],
+                get_font_formatting(script)
             ]
         ],
         [
             "all",
-            ["has", "name"],
-            ["has", "name2"],
-            ["!", ["has", "name3"]]
+            ["has", "pmap:pgf:name"],
+            ["has", "pmap:pgf:name2"],
+            ["!", ["has", "pmap:pgf:name3"]]
         ],
+        // The local name has 2 script segments: `name` and `name2`
         [
             "case",
             [
@@ -62,73 +108,51 @@ export function places_locality_text_field(
                 script === "Latin" ? ["has", "pmap:script"] : ["!=", ["get", "pmap:script"], script],
                 script === "Latin" ? ["has", "pmap:script2"] : ["!=", ["get", "pmap:script2"], script]
             ],
+            // Both `name` and `name2` are not in the target script
             [
                 "format",
-                [
-                    "coalesce",
-                    ["get", `name:${lang}`]
-                ],
-                {},
+                ["get", `pmap:pgf:name:${lang}`],
+                get_font_formatting(script),
                 "\n",
                 {},
-                ["get", "pmap:pgf:name"],
-                {
-                    "text-font": [
-                        "case",
-                        ["==", ["get", "pmap:script"], "Devanagari"],
-                        ["literal", ["Noto Sans Devanagari Regular v1"]],
-                        ["literal", ["Noto Sans Regular"]]
-                    ]
-                },
+                ...get_name_block('name'),
                 "\n",
                 {},
-                ["get", "pmap:pgf:name2"],
-                {
-                    "text-font": [
-                        "case",
-                        ["==", ["get", "pmap:script2"], "Devanagari"],
-                        ["literal", ["Noto Sans Devanagari Regular v1"]],
-                        ["literal", ["Noto Sans Regular"]]
-                    ]
-                }
+                ...get_name_block('name2')
             ],
+            // Either `name` or `name2` is in the target script
             [
                 "case",
                 script === "Latin" ? ["!", ["has", "pmap:script"]] : ["==", ["get", "pmap:script"], script],
+                // `name` is in the target script, and `name2` is not in the target script
                 [
                     "format",
-                    ["get", "name"],
-                    {},
+                    [
+                        "coalesce",
+                        ["get", `pmap:pgf:name:${lang}`],
+                        ["get", "pmap:pgf:name"]
+                    ],
+                    get_font_formatting(script),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name2"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script2"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    }
+                    ...get_name_block('name2')
                 ],
+                // `name2` is in the target script, and `name` is not in the target script
                 [
                     "format",
-                    ["get", "name2"],
-                    {},
+                    [
+                        "coalesce",
+                        ["get", `pmap:pgf:name:${lang}`],
+                        ["get", "pmap:pgf:name2"]
+                    ],
+                    get_font_formatting(script),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    }
+                    ...get_name_block('name')
                 ]
             ]
         ],
+        // The local name has 3 script segments: `name`, `name2`, and `name3`
         [
             "case",
             [
@@ -137,120 +161,73 @@ export function places_locality_text_field(
                 script === "Latin" ? ["has", "pmap:script2"] : ["!=", ["get", "pmap:script2"], script],
                 script === "Latin" ? ["has", "pmap:script2"] : ["!=", ["get", "pmap:script3"], script]
             ],
+            // All three `name`, `name2`, and `name3` are not in the target script
             [
                 "format",
-                [
-                    "coalesce",
-                    ["get", `name:${lang}`]
-                ],
-                {},
+                ["get", `pmap:pgf:name:${lang}`],
+                get_font_formatting(script),
                 "\n",
                 {},
-                ["get", "pmap:pgf:name"],
-                {
-                    "text-font": [
-                        "case",
-                        ["==", ["get", "pmap:script"], "Devanagari"],
-                        ["literal", ["Noto Sans Devanagari Regular v1"]],
-                        ["literal", ["Noto Sans Regular"]]
-                    ]
-                },
+                ...get_name_block('name'),
                 "\n",
                 {},
-                ["get", "pmap:pgf:name2"],
-                {
-                    "text-font": [
-                        "case",
-                        ["==", ["get", "pmap:script2"], "Devanagari"],
-                        ["literal", ["Noto Sans Devanagari Regular v1"]],
-                        ["literal", ["Noto Sans Regular"]]
-                    ]
-                }
+                ...get_name_block('name2'),
+                "\n",
+                {},
+                ...get_name_block('name3')
             ],
+            // Exactly one of the 3 script segments `name`, `name2`, or `name3` is in the target script
             [
                 "case",
                 script === "Latin" ? ["!", ["has", "pmap:script"]] : ["==", ["get", "pmap:script"], script],
+                // `name` is in the target script, and `name2` and `name3` are not
                 [
                     "format",
-                    ["get", "name"],
-                    {},
+                    [
+                        "coalesce",
+                        ["get", `pmap:pgf:name:${lang}`],
+                        ["get", "pmap:pgf:name"]
+                    ],
+                    get_font_formatting(script),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name2"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script2"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    },
+                    ...get_name_block('name2'),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name3"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script3"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    }
+                    ...get_name_block('name3')
                 ],
                 script === "Latin" ? ["!", ["has", "pmap:script2"]] : ["==", ["get", "pmap:script2"], script],
+                // `name2` is in the target script, and `name` and `name3` are not
                 [
                     "format",
-                    ["get", "name2"],
-                    {},
+                    [
+                        "coalesce",
+                        ["get", `pmap:pgf:name:${lang}`],
+                        ["get", "pmap:pgf:name2"]
+                    ],
+                    get_font_formatting(script),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    },
+                    ...get_name_block('name'),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name3"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script3"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    }
+                    ...get_name_block('name3')
                 ],
+                // `name3` is in the target script, and `name` and `name2` are not
                 [
                     "format",
-                    ["get", "name3"],
-                    {},
+                    [
+                        "coalesce",
+                        ["get", `pmap:pgf:name:${lang}`],
+                        ["get", "pmap:pgf:name3"]
+                    ],
+                    get_font_formatting(script),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    },
+                    ...get_name_block('name'),
                     "\n",
                     {},
-                    ["get", "pmap:pgf:name2"],
-                    {
-                        "text-font": [
-                            "case",
-                            ["==", ["get", "pmap:script2"], "Devanagari"],
-                            ["literal", ["Noto Sans Devanagari Regular v1"]],
-                            ["literal", ["Noto Sans Regular"]]
-                        ]
-                    }
+                    ...get_name_block('name2')
                 ]
             ]
         ]
