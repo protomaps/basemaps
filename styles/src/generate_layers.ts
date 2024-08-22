d99021ea3005f6cb57bb84609f8a718261863f90
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-08-22 16:58:20 +0200

    Default to English in generate_layers

--- styles/src/generate_layers.ts
+++ styles/src/generate_layers.ts
@@ -9,4 +9,7 @@ if (process.argv.length < 2) {
 }
 const args = process.argv.slice(2);
 
-process.stdout.write(JSON.stringify(i(args[0], args[1]), null, 2));
+const lang = 'en';
+const script = 'Latin';
+
+process.stdout.write(JSON.stringify(i(args[0], args[1], lang, script), null, 2));
