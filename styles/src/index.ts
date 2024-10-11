19a7164982609973fe96994afa70b6c226ab9eb7
Brandon Liu <bdon@users.noreply.github.com>
2024-10-11 15:07:10 +0800

    Typedoc (#319)
    * styles: export Theme and Pois interfaces
    
    fix CI

--- styles/src/index.ts
+++ styles/src/index.ts
@@ -1,9 +1,10 @@
 import { LayerSpecification } from "@maplibre/maplibre-gl-style-spec";
 import { labels_layers, nolabels_layers } from "./base_layers";
 import { language_script_pairs } from "./language";
-import themes, { Theme } from "./themes";
+import themes, { Theme, Pois } from "./themes";
 
 export { language_script_pairs };
+export type { Theme, Pois };
 
 export default function (
   source: string,
