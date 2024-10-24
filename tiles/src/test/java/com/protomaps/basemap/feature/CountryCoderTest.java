18b8d8b032ece468cc55341ff4fb27d4625769a6
Brandon Liu <bdon@bdon.org>
2024-10-23 20:06:24 -0700

    linting

--- tiles/src/test/java/com/protomaps/basemap/feature/CountryCoderTest.java
+++ tiles/src/test/java/com/protomaps/basemap/feature/CountryCoderTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
-public class CountryCoderTest {
+class CountryCoderTest {
 
   @Test
   void testLookupByPoint() throws IOException {
