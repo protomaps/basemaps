f40ab9efcdc61623f3dbf19c5e517348b34e2967
Brandon Liu <bdon@users.noreply.github.com>
2024-11-10 12:17:23 -0800

    fix ordering of builds, latest to oldest, on chrome. (#332)
    * fix ordering of builds, latest to oldest, on chrome.

--- app/src/BuildsComponent.tsx
+++ app/src/BuildsComponent.tsx
@@ -164,7 +164,9 @@ export default function BuildsComponent() {
       .then((j) => {
         setBuilds(
           j.sort((a: Build, b: Build) => {
-            return a.key < b.key;
+            if (a.key < b.key) return 1;
+            if (a.key > b.key) return -1;
+            return 0;
           }),
         );
       });
