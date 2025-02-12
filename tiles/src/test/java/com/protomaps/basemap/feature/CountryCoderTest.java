1fcd95222f976e9e4cde9d0427b1c20728e0e3ae
Brandon Liu <bdon@bdon.org>
2025-02-12 18:48:44 +0800

    linting

--- tiles/src/test/java/com/protomaps/basemap/feature/CountryCoderTest.java
+++ tiles/src/test/java/com/protomaps/basemap/feature/CountryCoderTest.java
@@ -10,42 +10,42 @@ import org.junit.jupiter.api.Test;
 class CountryCoderTest {
 
   @Test
-  void testLookupByPoint() throws IOException {
+  void testLookupByPoint() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"iso1A2\":\"GB\",\"nameEn\":\"Great Britain\"},\"geometry\":{\"type\":\"MultiPolygon\",\"coordinates\":[[[[0,0],[0,1],[1,1],[0,0]]]]}}]}");
     assertEquals(Optional.of("GB"), c.getCountryCode(newPoint(0.1, 0.9)));
   }
 
   @Test
-  void testLookupByPointActuallyContains() throws IOException {
+  void testLookupByPointActuallyContains() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"iso1A2\":\"GB\",\"nameEn\":\"Great Britain\"},\"geometry\":{\"type\":\"MultiPolygon\",\"coordinates\":[[[[0,0],[0,1],[1,1],[0,0]]]]}}]}");
     assertEquals(Optional.empty(), c.getCountryCode(newPoint(0.9, 0.1)));
   }
 
   @Test
-  void testNullGeometry() throws IOException {
+  void testNullGeometry() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"iso1A2\":\"xx\",\"nameEn\":\"Iberia\"},\"geometry\":null}]}");
     assertEquals(Optional.empty(), c.getCountryCode(newPoint(0.1, 0.1)));
   }
 
   @Test
-  void testNoGeometry() throws IOException {
+  void testNoGeometry() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"iso1A2\":\"xx\",\"nameEn\":\"Iberia\"}}]}");
     assertEquals(Optional.empty(), c.getCountryCode(newPoint(0.1, 0.1)));
   }
 
   @Test
-  void testNoCountry() throws IOException {
+  void testNoCountry() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"nameEn\":\"Iberia\"},\"geometry\":{\"type\":\"MultiPolygon\",\"coordinates\":[[[[0,0],[0,1],[1,1],[0,0]]]]}}]}");
     assertEquals(Optional.empty(), c.getCountryCode(newPoint(0.1, 0.1)));
   }
 
   @Test
-  void testDependency() throws IOException {
+  void testDependency() {
     CountryCoder c = CountryCoder.fromJsonString(
       "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"country\":\"GB\",\"nameEn\":\"Great Britain\"},\"geometry\":{\"type\":\"MultiPolygon\",\"coordinates\":[[[[0,0],[0,1],[1,1],[0,0]]]]}}]}");
     assertEquals(Optional.of("GB"), c.getCountryCode(newPoint(0.1, 0.9)));
