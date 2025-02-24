f35e660c3c604ab9e5fb9d0399830721a0913abc
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-02-24 11:26:23 +0100

    Add test

--- tiles/src/test/java/com/protomaps/basemap/layers/RoadsTest.java
+++ tiles/src/test/java/com/protomaps/basemap/layers/RoadsTest.java
@@ -1,8 +1,12 @@
 package com.protomaps.basemap.layers;
 
 import static com.onthegomap.planetiler.TestUtils.newLineString;
+import static com.onthegomap.planetiler.TestUtils.newPoint;
 
 import com.onthegomap.planetiler.reader.SimpleFeature;
+import com.onthegomap.planetiler.reader.osm.OsmElement;
+import com.onthegomap.planetiler.reader.osm.OsmReader;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,4 +30,24 @@ class RoadsTest extends LayerTest {
         0
       )));
   }
+
+  // @Test
+  // void relation() {
+  //   var interstateRelation = new OsmElement.Relation(1);
+  //   interstateRelation.setTag("route", "road");
+  //   interstateRelation.setTag("type", "route");
+  //   interstateRelation.setTag("networks", "US:I");
+
+  //   assertFeatures(3,
+  //     List.of(Map.of("kind", "highway")),
+  //     process(SimpleFeature.createFakeOsmFeature(
+  //       newLineString(0, 10, 0, 20),
+  //       new HashMap<>(Map.of("highway", "motorway")),
+  //       "osm",
+  //       null,
+  //       0,
+  //       profile.preprocessOsmRelation(interstateRelation).stream().map(r -> new OsmReader.RelationMember<>("", r)).toList()
+  //     ))
+  //   );
+  // }
 }
