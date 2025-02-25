d6c814e47e8ee0b6f72953735e51444aff072448
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-02-25 19:35:02 +0100

    Use Matcher on landuse layer

--- tiles/src/main/java/com/protomaps/basemap/feature/Matcher.java
+++ tiles/src/main/java/com/protomaps/basemap/feature/Matcher.java
@@ -3,14 +3,12 @@ package com.protomaps.basemap.feature;
 import com.onthegomap.planetiler.expression.Expression;
 import com.onthegomap.planetiler.expression.MultiExpression;
 import com.onthegomap.planetiler.reader.SourceFeature;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Matcher {
-
   public record Use(String key, Object value) {}
 
   public static MultiExpression.Entry<Map<String, Object>> entry(Object... arguments) {
@@ -33,10 +31,9 @@ public class Matcher {
   public static Expression with(String... arguments) {
     List<String> argumentList = List.of(arguments);
     if (argumentList.isEmpty()) {
-        return Expression.TRUE;
-    }
-    else if (argumentList.size() == 1) {
-        return Expression.matchField(argumentList.getFirst());
+      return Expression.TRUE;
+    } else if (argumentList.size() == 1) {
+      return Expression.matchField(argumentList.getFirst());
     }
     return Expression.matchAny(argumentList.getFirst(), argumentList.subList(1, argumentList.size()));
   }
@@ -46,66 +43,59 @@ public class Matcher {
   }
 
   public record FromTag(String key) {}
+
   public static FromTag fromTag(String key) {
     return new FromTag(key);
   }
 
   public static String getString(SourceFeature sf, List<Map<String, Object>> matches, String key, String defaultValue) {
     for (var match : matches.reversed()) {
-        if (match.containsKey(key)) {
-            Object value = match.get(key);
-            if (value instanceof String stringValue) {
-                return stringValue;
-            }
-            else if (value instanceof FromTag fromTag) {
-                return sf.getString(fromTag.key);
-            }
-            else {
-                return defaultValue;
-            }
+      if (match.containsKey(key)) {
+        Object value = match.get(key);
+        if (value instanceof String stringValue) {
+          return stringValue;
+        } else if (value instanceof FromTag fromTag) {
+          return sf.getString(fromTag.key);
+        } else {
+          return defaultValue;
         }
+      }
     }
     return defaultValue;
   }
 
-  public static Integer getInteger(SourceFeature sf, List<Map<String, Object>> matches, String key, Integer defaultValue) {
+  public static Integer getInteger(SourceFeature sf, List<Map<String, Object>> matches, String key,
+    Integer defaultValue) {
     for (var match : matches.reversed()) {
-        if (match.containsKey(key)) {
-            Object value = match.get(key);
-            if (value instanceof Integer integerValue) {
-                return integerValue;
-            }
-            else if (value instanceof FromTag fromTag) {
-                return Integer.valueOf(sf.getString(fromTag.key, String.valueOf(defaultValue)));
-            }
-            else {
-                return defaultValue;
-            }
+      if (match.containsKey(key)) {
+        Object value = match.get(key);
+        if (value instanceof Integer integerValue) {
+          return integerValue;
+        } else if (value instanceof FromTag fromTag) {
+          return Integer.valueOf(sf.getString(fromTag.key, String.valueOf(defaultValue)));
+        } else {
+          return defaultValue;
         }
+      }
     }
     return defaultValue;
   }
 
-  public static Boolean getBoolean(SourceFeature sf, List<Map<String, Object>> matches, String key, Boolean defaultValue) {
+  public static Boolean getBoolean(SourceFeature sf, List<Map<String, Object>> matches, String key,
+    Boolean defaultValue) {
     for (var match : matches.reversed()) {
-        if (match.containsKey(key)) {
-            Object value = match.get(key);
-            if (value instanceof Boolean booleanValue) {
-                return booleanValue;
-            }
-            else if (value instanceof FromTag fromTag) {
-                return sf.hasTag(fromTag.key) ? sf.getBoolean(fromTag.key) : defaultValue;
-            }
-            else {
-                return defaultValue;
-            }
+      if (match.containsKey(key)) {
+        Object value = match.get(key);
+        if (value instanceof Boolean booleanValue) {
+          return booleanValue;
+        } else if (value instanceof FromTag fromTag) {
+          return sf.hasTag(fromTag.key) ? sf.getBoolean(fromTag.key) : defaultValue;
+        } else {
+          return defaultValue;
         }
+      }
     }
     return defaultValue;
   }
 
-  public static void main() {
-    use("hi", 15);
-    use("some", "thing");
-  }
 }
--- tiles/src/main/java/com/protomaps/basemap/layers/Landuse.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Landuse.java
@@ -1,154 +1,224 @@
 package com.protomaps.basemap.layers;
 
+import static com.protomaps.basemap.feature.Matcher.entry;
+import static com.protomaps.basemap.feature.Matcher.fromTag;
+import static com.protomaps.basemap.feature.Matcher.getString;
+import static com.protomaps.basemap.feature.Matcher.use;
+import static com.protomaps.basemap.feature.Matcher.with;
+import static com.protomaps.basemap.feature.Matcher.without;
+
 import com.onthegomap.planetiler.FeatureCollector;
 import com.onthegomap.planetiler.FeatureMerge;
 import com.onthegomap.planetiler.ForwardingProfile;
 import com.onthegomap.planetiler.VectorTile;
+import com.onthegomap.planetiler.expression.MultiExpression;
 import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import com.protomaps.basemap.feature.FeatureId;
 import com.protomaps.basemap.postprocess.Area;
 import java.util.List;
+import java.util.Map;
+
 
 public class Landuse implements ForwardingProfile.LayerPostProcessor {
 
+  private static final MultiExpression.Index<Map<String, Object>> index = MultiExpression.of(List.of(
+    entry(
+      with("area:aeroway", "taxiway", "runway"),
+      use("kind", "other")
+    ),
+    entry(
+      with("aeroway", "aerodrome", "runway"),
+      use("kind", fromTag("aeroway"))
+    ),
+    entry(
+      with("amenity", "hospital", "school", "kindergarten", "university", "college", "library", "post_office",
+        "townhall", "cafe"),
+      use("kind", fromTag("amenity"))
+    ),
+    entry(
+      with("boundary", "national_park", "protected_area"),
+      use("kind", fromTag("boundary"))
+    ),
+    entry(
+      with("landuse", "recreation_ground", "railway", "commercial", "grass", "forest", "meadow", "grass"),
+      use("kind", "other")
+    ),
+    entry(
+      with("landuse", "cemetery", "residential", "village_green", "allotments", "military"),
+      use("kind", fromTag("landuse"))
+    ),
+    entry(
+      with("landuse", "orchard", "farmland", "farmyard"),
+      use("kind", "farmland")
+    ),
+    entry(
+      with("landuse", "industrial", "brownfield"),
+      use("kind", "industrial")
+    ),
+    entry(
+      with("landuse", "military"),
+      with("military", "naval_base", "airfield"),
+      use("kind", fromTag("military"))
+    ),
+    entry(
+      with("leisure", "garden", "dog_park", "playground", "pitch", "nature_reserve"),
+      use("kind", "other")
+    ),
+    entry(
+      with("leisure", "golf_course", "marina", "park", "stadium"),
+      use("kind", fromTag("leisure"))
+    ),
+    entry(
+      with("man_made", "bridge"),
+      use("kind", "pedestrian")
+    ),
+    entry(
+      with("man_made", "pier"),
+      use("kind", "pier")
+    ),
+    entry(
+      with("natural", "beach", "wood", "glacier", "grass", "scrub", "sand", "wetland", "bare_rock"),
+      use("kind", fromTag("natural"))
+    ),
+    entry(
+      with("place", "neighbourhood"),
+      use("kind", "other")
+    ),
+    entry(
+      with("railway", "platform"),
+      use("kind", "platform")
+    ),
+    entry(
+      with("tourism", "zoo"),
+      use("kind", "other")
+    ),
+    entry(
+      with("tourism", "attraction", "camp_site", "hotel"),
+      use("kind", fromTag("tourism"))
+    ),
+    entry(
+      with("area", "yes"),
+      with("highway", "pedestrian", "footway"),
+      use("kind", "pedestrian")
+    ),
+    entry(
+      with("shop", "grocery", "supermarket"),
+      use("kind", fromTag("shop"))
+    ),
+    entry(
+      with("boundary", "national_park"),
+      with("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      use("kind", "forest")
+    ),
+    entry(
+      with("boundary", "national_park"),
+      with("protect_class", "6"),
+      with("protection_title", "National Forest"),
+      use("kind", "forest")
+    ),
+    entry(
+      with("landuse", "forest"),
+      with("protect_class", "6"),
+      use("kind", "forest")
+    ),
+    entry(
+      with("landuse", "forest"),
+      with("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      use("kind", "forest")
+    ),
+    entry(
+      with("landuse", "forest"),
+      use("kind", "forest")
+    ),
+    entry(
+      with("boundary", "national_park"),
+      use("kind", "park")
+    ),
+    entry(
+      with("boundary", "protected_area"),
+      with("protect_class", "6"),
+      with("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      use("kind", "forest")
+    ),
+    entry(
+      without("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      without("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
+        "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
+        "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
+        "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
+        "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary"),
+      with("protect_class", "2", "3"),
+      use("kind", "national_park")
+    ),
+    entry(
+      without("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      without("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
+        "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
+        "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
+        "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
+        "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary"),
+      with("operator", "United States National Park Service", "National Park Service",
+        "US National Park Service", "U.S. National Park Service", "US National Park service"),
+      use("kind", "national_park")
+    ),
+    entry(
+      without("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      without("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
+        "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
+        "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
+        "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
+        "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary"),
+      with("operator:en", "Parks Canada"),
+      use("kind", "national_park")
+    ),
+    entry(
+      without("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      without("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
+        "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
+        "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
+        "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
+        "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary"),
+      with("designation", "national_park"),
+      use("kind", "national_park")
+    ),
+    entry(
+      without("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
+        "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
+        "United State Forest Service", "U.S. National Forest Service"),
+      without("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
+        "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
+        "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
+        "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
+        "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary"),
+      with("protection_title", "National Park"),
+      use("kind", "national_park")
+    )
+  )).index();
+
   public void processOsm(SourceFeature sf, FeatureCollector features) {
-    if (sf.canBePolygon() && (sf.hasTag("aeroway", "aerodrome", "runway") ||
-      sf.hasTag("area:aeroway", "taxiway", "runway") ||
-      sf.hasTag("amenity", "hospital", "school", "kindergarten", "university", "college") ||
-      sf.hasTag("boundary", "national_park", "protected_area") ||
-      sf.hasTag("landuse", "recreation_ground", "industrial", "brownfield", "railway", "cemetery", "commercial",
-        "grass", "orchard", "farmland", "farmyard", "residential", "military", "village_green", "allotments", "forest",
-        "meadow", "grass") ||
-      sf.hasTag("leisure", "park", "garden", "golf_course", "dog_park", "playground", "pitch", "nature_reserve") ||
-      sf.hasTag("man_made", "pier", "bridge") ||
-      sf.hasTag("natural", "beach", "wood", "glacier", "grass", "scrub", "sand", "wetland", "bare_rock") ||
-      // TODO: (nvkelso 20230622) This use of the place tag here is dubious, though paired with "residential"
-      sf.hasTag("place", "neighbourhood") ||
-      sf.hasTag("railway", "platform") ||
-      sf.hasTag("tourism", "zoo") ||
-      (sf.hasTag("area", "yes") &&
-        (sf.hasTag("highway", "pedestrian", "footway"))))) {
-      String kind = "other";
-      if (sf.hasTag("aeroway", "aerodrome")) {
-        kind = sf.getString("aeroway");
-      } else if (sf.hasTag("amenity", "university", "college", "hospital", "library", "post_office", "school",
-        "townhall")) {
-        kind = sf.getString("amenity");
-      } else if (sf.hasTag("amenity", "cafe")) {
-        kind = sf.getString("amenity");
-      } else if (sf.hasTag("highway")) {
-        kind = "pedestrian";
-      } else if (sf.hasTag("landuse", "cemetery")) {
-        kind = sf.getString("landuse");
-      } else if (sf.hasTag("landuse", "orchard", "farmland", "farmyard")) {
-        kind = "farmland";
-      } else if (sf.hasTag("landuse", "residential")) {
-        kind = "residential";
-      } else if (sf.hasTag("landuse", "village_green")) {
-        kind = "village_green";
-      } else if (sf.hasTag("landuse", "allotments")) {
-        kind = "allotments";
-      } else if (sf.hasTag("landuse", "industrial", "brownfield")) {
-        kind = "industrial";
-      } else if (sf.hasTag("landuse", "military")) {
-        kind = "military";
-        if (sf.hasTag("military", "naval_base", "airfield")) {
-          kind = sf.getString("military");
-        }
-      } else if (sf.hasTag("leisure", "golf_course", "marina", "park", "stadium")) {
-        kind = sf.getString("leisure");
-      } else if (sf.hasTag("man_made", "bridge")) {
-        kind = "pedestrian";
-      } else if (sf.hasTag("man_made", "pier")) {
-        kind = "pier";
-      } else if (sf.hasTag("shop", "grocery", "supermarket")) {
-        kind = sf.getString("shop");
-      } else if (sf.hasTag("tourism", "attraction", "camp_site", "hotel")) {
-        kind = sf.getString("tourism");
-      } else {
-        // Avoid problem of too many "other" kinds
-        // All these will default to min_zoom of 15
-        // If a more specific min_zoom is needed (or sanitize kind values)
-        // then add new logic in section above
-        if (sf.hasTag("amenity")) {
-          kind = sf.getString("amenity");
-        } else if (sf.hasTag("craft")) {
-          kind = sf.getString("craft");
-        } else if (sf.hasTag("aeroway")) {
-          kind = sf.getString("aeroway");
-        } else if (sf.hasTag("historic")) {
-          kind = sf.getString("historic");
-        } else if (sf.hasTag("landuse")) {
-          kind = sf.getString("landuse");
-        } else if (sf.hasTag("leisure")) {
-          kind = sf.getString("leisure");
-        } else if (sf.hasTag("man_made")) {
-          kind = sf.getString("man_made");
-        } else if (sf.hasTag("natural")) {
-          kind = sf.getString("natural");
-        } else if (sf.hasTag("railway")) {
-          kind = sf.getString("railway");
-        } else if (sf.hasTag("shop")) {
-          kind = sf.getString("shop");
-        } else if (sf.hasTag("tourism")) {
-          kind = sf.getString("tourism");
-          // Boundary is most generic, so place last else we loose out
-          // on nature_reserve detail versus all the protected_area
-        } else if (sf.hasTag("boundary")) {
-          kind = sf.getString("boundary");
-        }
+    if (sf.canBePolygon()) {
+
+      var matches = index.getMatches(sf);
+      if (matches.isEmpty()) {
+        return;
       }
 
-      // National forests
-      if (sf.hasTag("boundary", "national_park") &&
-        sf.hasTag("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
-          "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
-          "United State Forest Service", "U.S. National Forest Service")) {
-        kind = "forest";
-      } else if (sf.hasTag("boundary", "national_park") &&
-        sf.hasTag("protect_class", "6") &&
-        sf.hasTag("protection_title", "National Forest")) {
-        kind = "forest";
-      } else if (sf.hasTag("landuse", "forest") &&
-        sf.hasTag("protect_class", "6")) {
-        kind = "forest";
-      } else if (sf.hasTag("landuse", "forest") &&
-        sf.hasTag("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
-          "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
-          "United State Forest Service", "U.S. National Forest Service")) {
-        kind = "forest";
-      } else if (sf.hasTag("landuse", "forest")) {
-        kind = "forest";
-      } else if (sf.hasTag("boundary", "protected_area") &&
-        sf.hasTag("protect_class", "6") &&
-        sf.hasTag("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
-          "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
-          "United State Forest Service", "U.S. National Forest Service")) {
-        kind = "forest";
-      }
-
-      // National parks
-      if (sf.hasTag("boundary", "national_park")) {
-        if (!(sf.hasTag("operator", "United States Forest Service", "US Forest Service", "U.S. Forest Service",
-          "USDA Forest Service", "United States Department of Agriculture", "US National Forest Service",
-          "United State Forest Service", "U.S. National Forest Service") ||
-          sf.hasTag("protection_title", "Conservation Area", "Conservation Park", "Environmental use", "Forest Reserve",
-            "National Forest", "National Wildlife Refuge", "Nature Refuge", "Nature Reserve", "Protected Site",
-            "Provincial Park", "Public Access Land", "Regional Reserve", "Resources Reserve", "State Forest",
-            "State Game Land", "State Park", "Watershed Recreation Unit", "Wild Forest", "Wilderness Area",
-            "Wilderness Study Area", "Wildlife Management", "Wildlife Management Area", "Wildlife Sanctuary")) &&
-          (sf.hasTag("protect_class", "2", "3") ||
-            sf.hasTag("operator", "United States National Park Service", "National Park Service",
-              "US National Park Service", "U.S. National Park Service", "US National Park service") ||
-            sf.hasTag("operator:en", "Parks Canada") ||
-            sf.hasTag("designation", "national_park") ||
-            sf.hasTag("protection_title", "National Park"))) {
-          kind = "national_park";
-        } else {
-          kind = "park";
-        }
-      }
+      String kind = getString(sf, matches, "kind", "other");
 
       features.polygon(this.name())
         .setId(FeatureId.create(sf))
