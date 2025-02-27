8db866606aaf188216738e5181b47b75a8af4ec6
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-02-27 10:40:48 +0100

    Add tests

--- tiles/src/test/java/com/protomaps/basemap/layers/RoadsTest.java
+++ tiles/src/test/java/com/protomaps/basemap/layers/RoadsTest.java
@@ -29,6 +29,34 @@ class RoadsTest extends LayerTest {
     ));
   }
 
+  private FeatureCollector processWithRelationAndCoords(String network, double startLon, double startLat, double endLon,
+    double endLat, String... arguments) {
+    var relationResult = profile.preprocessOsmRelation(new OsmElement.Relation(1, Map.of(
+      "type", "route",
+      "route", "road",
+      "network", network
+    ), List.of(
+      new OsmElement.Relation.Member(OsmElement.Type.WAY, 2, "role")
+    )));
+
+    Map<String, Object> tags = new HashMap<>();
+    List<String> argumentList = List.of(arguments);
+    if (argumentList.size() % 2 == 0) {
+      for (int i = 0; i < argumentList.size(); i += 2) {
+        tags.put(argumentList.get(i), argumentList.get(i + 1));
+      }
+    }
+
+    return process(SimpleFeature.createFakeOsmFeature(
+      newLineString(startLon, startLat, endLon, endLat),
+      tags,
+      "osm",
+      null,
+      2,
+      relationResult.stream().map(info -> new OsmReader.RelationMember<>("role", info)).toList()
+    ));
+  }
+
   @Test
   void simple() {
     assertFeatures(12,
@@ -48,113 +76,125 @@ class RoadsTest extends LayerTest {
   @Test
   void relation1() {
     // highway=motorway is part of a US Interstate relation and is located in the US -> minzoom should be 3
-    var relationResult = profile.preprocessOsmRelation(new OsmElement.Relation(1, Map.of(
-      "type", "route",
-      "route", "road",
-      "network", "US:I"
-    ), List.of(
-      new OsmElement.Relation.Member(OsmElement.Type.WAY, 2, "role")
-    )));
-
-    FeatureCollector features = process(SimpleFeature.createFakeOsmFeature(
-      newLineString(-104.97235, 39.73867, -105.260503, 40.010771), // Denver - Boulder
-      new HashMap<>(Map.of("highway", "motorway")),
-      "osm",
-      null,
-      2,
-      relationResult.stream().map(info -> new OsmReader.RelationMember<>("role", info)).toList()
-    ));
-
+    // Denver - Boulder
     assertFeatures(0,
       List.of(Map.of(
         "_minzoom", 3
       )),
-      features
+      processWithRelationAndCoords("US:I", -104.97235, 39.73867, -105.260503, 40.010771, "highway", "motorway")
     );
   }
 
   @Test
   void relation2() {
     // highway=motorway is part of US State network and is located in the US -> minzoom should be 6
-    var relationResult = profile.preprocessOsmRelation(new OsmElement.Relation(1, Map.of(
-      "type", "route",
-      "route", "road",
-      "network", "US:US"
-    ), List.of(
-      new OsmElement.Relation.Member(OsmElement.Type.WAY, 2, "role")
-    )));
-
-    FeatureCollector features = process(SimpleFeature.createFakeOsmFeature(
-      newLineString(-104.97235, 39.73867, -105.260503, 40.010771), // Denver - Boulder
-      new HashMap<>(Map.of("highway", "motorway")),
-      "osm",
-      null,
-      2,
-      relationResult.stream().map(info -> new OsmReader.RelationMember<>("role", info)).toList()
-    ));
-
+    // Denver - Boulder
     assertFeatures(0,
       List.of(Map.of(
         "_minzoom", 6
       )),
-      features
+      processWithRelationAndCoords("US:US", -104.97235, 39.73867, -105.260503, 40.010771, "highway", "motorway")
     );
   }
 
   @Test
   void relation3() {
     // highway=motorway is not part of US Interstate/State network and is located in the US -> minzoom should be 7
-    var relationResult = profile.preprocessOsmRelation(new OsmElement.Relation(1, Map.of(
-      "type", "route",
-      "route", "road",
-      "network", "some:network"
-    ), List.of(
-      new OsmElement.Relation.Member(OsmElement.Type.WAY, 2, "role")
-    )));
-
-    FeatureCollector features = process(SimpleFeature.createFakeOsmFeature(
-      newLineString(-104.97235, 39.73867, -105.260503, 40.010771), // Denver - Boulder
-      new HashMap<>(Map.of("highway", "motorway")),
-      "osm",
-      null,
-      2,
-      relationResult.stream().map(info -> new OsmReader.RelationMember<>("role", info)).toList()
-    ));
-
+    // Denver - Boulder
     assertFeatures(0,
       List.of(Map.of(
         "_minzoom", 7
       )),
-      features
+      processWithRelationAndCoords("some:network", -104.97235, 39.73867, -105.260503, 40.010771, "highway", "motorway")
     );
   }
 
   @Test
   void relation4() {
     // highway=motorway is part of US State network and is located ouside of the US -> minzoom should be 3
-    var relationResult = profile.preprocessOsmRelation(new OsmElement.Relation(1, Map.of(
-      "type", "route",
-      "route", "road",
-      "network", "US:US"
-    ), List.of(
-      new OsmElement.Relation.Member(OsmElement.Type.WAY, 2, "role")
-    )));
-
-    FeatureCollector features = process(SimpleFeature.createFakeOsmFeature(
-      newLineString(2.424, 48.832, 8.52332, 47.36919), // Paris - Zurich
-      new HashMap<>(Map.of("highway", "motorway")),
-      "osm",
-      null,
-      2,
-      relationResult.stream().map(info -> new OsmReader.RelationMember<>("role", info)).toList()
-    ));
-
+    // Paris - Zurich
     assertFeatures(0,
       List.of(Map.of(
         "_minzoom", 3
       )),
-      features
+      processWithRelationAndCoords("US:US", 2.424, 48.832, 8.52332, 47.36919, "highway", "motorway")
     );
   }
 
+  @Test
+  void testHighwayExcluded() {
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "proposed")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "abandoned")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "razed")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "demolished")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "removed")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "construction")
+    );
+
+    assertFeatures(12,
+      List.of(),
+      processWith("highway", "elevator")
+    );
+  }
+
+  @Test
+  void testHighwayOther() {
+    assertFeatures(12,
+      List.of(Map.of("kind", "other",
+        "kind_detail", "a",
+        "_minzoom", 14
+      )),
+      processWith("highway", "a")
+    );
+
+    assertFeatures(12,
+      List.of(Map.of("kind", "other",
+        "kind_detail", "b",
+        "_minzoom", 14
+      )),
+      processWith("highway", "a",
+        "service", "b")
+    );
+  }
+
+  @Test
+  void testHighwayMotorway() {
+    assertFeatures(12,
+      List.of(Map.of("kind", "highway",
+        "kind_detail", "motorway",
+        "_minzoom", 3
+      )),
+      processWith("highway", "motorway")
+    );
+
+    assertFeatures(12,
+      List.of(Map.of("kind", "highway",
+        "kind_detail", "motorway",
+        "_minzoom", 3
+      )),
+      processWith("highway", "motorway")
+    );
+  }
 }
