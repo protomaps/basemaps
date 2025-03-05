87a794a75ecec11ab522765c7dab5348a90d9093
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-03-05 10:24:00 +0100

    Add comment

--- tiles/src/main/java/com/protomaps/basemap/layers/Water.java
+++ tiles/src/main/java/com/protomaps/basemap/layers/Water.java
@@ -13,7 +13,7 @@ import com.protomaps.basemap.names.NeNames;
 import com.protomaps.basemap.names.OsmNames;
 import java.util.List;
 
-@SuppressWarnings("java:S1192")
+@SuppressWarnings("java:S1192") // Duplicated string literals
 public class Water implements ForwardingProfile.LayerPostProcessor {
 
   private static final double WORLD_AREA_FOR_70K_SQUARE_METERS =
