e9490ec23db76ae5b9eb1f9ed0168e5daf7d7443
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-04-23 11:18:48 +0200

    Add OsmTags class

---
+++ tiles/src/main/java/com/protomaps/basemap/feature/OsmTags.java
@@ -0,0 +1,11 @@
+package com.protomaps.basemap.feature;
+
+public final class OsmTags {
+    private OsmTags() {}
+
+    public static final String HIGHWAY = "highway";
+    public static final class HighwayValues {
+        public static final String MOTORWAY = "motorway";
+        private HighwayValues() {}
+    }
+}
