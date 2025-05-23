5afd52a09d67ec9fa0a525aa5432f99380e0fc8c
Brandon Liu <bdon@users.noreply.github.com>
2025-05-23 08:05:50 -0700

    Check for Basemap.java version bump in CI (#483)
    * Check for Basemap.java version bump in CI
    
    * move to external bash script

---
+++ .github/workflows/check-version-bump.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CHANGED_FILES=$(git diff --name-only $1...HEAD)
+echo "$CHANGED_FILES"
+TILES_SRC_CHANGED=$(echo "$CHANGED_FILES" | grep '^tiles/src/main' || true)
+VERSION_CHANGED=$(echo "$CHANGED_FILES" | grep '^tiles/src/main/java/com/protomaps/basemap/Basemap.java$' || true)
+if [[ -n "$TILES_SRC_CHANGED" && -z "$VERSION_CHANGED" ]]; then
+  echo "::error ::You modified files in tiles/src/main, but did not update Basemap.java."
+  exit 1
+fi
