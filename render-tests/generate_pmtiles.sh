7266a0bf9aa4bad58adf96666682251b4b2a78d2
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-05-23 11:01:53 +0200

    Render all layers

--- render-tests/generate_pmtiles.sh
+++ render-tests/generate_pmtiles.sh
@@ -5,5 +5,5 @@ java -Xmx2g -jar target/protomaps-basemap-HEAD-with-deps.jar \
     --osm_path=../render-tests/fixtures/monaco-2025-02-15.osm.pbf \
     --output=../render-tests/pmtiles/monaco-roads.pmtiles \
     --force \
-    --nodemap-type=sparsearray \
-    --layer=roads
+    --download \
+    --nodemap-type=sparsearray
