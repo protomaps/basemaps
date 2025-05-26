8697dd372c22cef7a29086620e57c8dfaab907a4
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-05-26 08:45:07 +0200

    Merge inputs

--- render-tests/generate_pmtiles.sh
+++ render-tests/generate_pmtiles.sh
@@ -1,9 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+mapfile -t input_files < <(find tests/ -type f -name "input.osm.pbf")
+
+sorted_files=()
+
+for input in "${input_files[@]}"; do
+  dir=$(dirname "$input")
+  sorted="$dir/input-sorted.osm.pbf"
+  echo "Sorting $input..."
+  osmium sort --overwrite "$input" -o "$sorted"
+  sorted_files+=("$sorted")
+done
+
+echo "Merging..."
+osmium merge --overwrite "${sorted_files[@]}" -o merged.osm.pbf
+
 mkdir -p pmtiles
 cd ../tiles
 mvn clean package -DskipTests
 java -Xmx2g -jar target/protomaps-basemap-HEAD-with-deps.jar \
-    --osm_path=../render-tests/fixtures/monaco-2025-02-15.osm.pbf \
+    --osm_path=../render-tests/merged.osm.pbf \
     --output=../render-tests/pmtiles/monaco-roads.pmtiles \
     --force \
     --download \
+    --keep_unzipped \
     --nodemap-type=sparsearray
